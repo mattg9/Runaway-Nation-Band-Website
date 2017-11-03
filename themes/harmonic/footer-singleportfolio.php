93662bb1f3153115f0d0880490dd6bbe1a11c977
Matthew Gaston <matthewgaston9@gmail.com>
2017-11-03 13:57:28 -0400

    first commit

---
+++ themes/harmonic/footer-singleportfolio.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package harmonic
+ */
+?>
+
+						<div class="clear"></div>
+					</div><!-- #content .site-wrapper -->
+				</div><!-- #content-wrapper -->
+			</div>
+			<footer id="colophon" class="site-footer" role="contentinfo">
+
+				<div class="site-info">
+					<p> Runaway Nation &copy; 2017 <p>
+				</div><!-- .site-info -->
+
+			</footer><!-- #colophon .site-footer -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
